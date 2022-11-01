c49813e26f746a143d8103d4d05f2c2ef80b7548
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-11-01 07:45:01 +0530

    Add main app

---
+++ src/main/java/com/iam2kabhishek/filejump/SpringBootUploadFilesDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.iam2kabhishek.filejump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadFilesDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUploadFilesDatabaseApplication.class, args);
+	}
+
+}
